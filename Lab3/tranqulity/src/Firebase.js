4d04c35c1eaff9c927346c8aef7fe37c87384c00
unknown <dziedziakmichal0@gmail.com>
2024-05-14 11:32:31 +0200

    firebase deploy

---
+++ Lab3/tranqulity/src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBV_OlKPWm_xY1J3XAaYDIrgjpv4lG32ow",
+  authDomain: "piwo-lab-d7ee8.firebaseapp.com",
+  projectId: "piwo-lab-d7ee8",
+  storageBucket: "piwo-lab-d7ee8.appspot.com",
+  messagingSenderId: "148024221661",
+  appId: "1:148024221661:web:873d3bafa443b7c33ca034",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Dane
+
 Michał Dziedziak 263901
+
 # Termin
+
 Wtorek Parzysty 13:15 - 15:00
+
+# Firebase
+
+Deploy: [piwo-lab-d7ee8.web.app](piwo-lab-d7ee8.web.app)
